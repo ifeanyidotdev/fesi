50f95fef44a414a3c155ea5b1e431f447c36b41b
ifeanyidotdev <mremmalex2@gmail.com>
2025-07-24 00:52:19 +0100

    added sub command for the run command

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 use std::{env, fs, path, process};
 
+const FESI_DIR_NAME: &str = "FESI";
 const FESI_HELP_MESSAGE: &str = r#"
 Fesi is a drop in replacement for curl
 
 Usage: fesi [OPTIONS] <COMMAND>
 
 Commands:
-    init   Initialize a new fesi project in the current direcotry
+    init              Initialize a new fesi project in the current direcotry
+    run               Run endpoint request
 
 Optoons:
     -h, --help        Prints the help message
     -v, --version     Prints version informations
-    -m, --method      which http method to run the request with
 "#;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() <= 1 {
+        println!("{FESI_HELP_MESSAGE}");
+        process::exit(1)
+    }
+
+    let sub_commands = &args[2..];
+    //parse arguments
     for arg in args.iter().skip(1) {
         match arg.as_str() {
             "-h" | "--help" => {
@@ -24,7 +32,10 @@ fn main() {
                 process::exit(0)
             }
             "init" => {
-                initialize_fesi();
+                initialize_fesi_project();
+            }
+            "run" => {
+                handle_run_command(sub_commands);
             }
             _ => {
                 println!("{FESI_HELP_MESSAGE}");
@@ -34,15 +45,93 @@ fn main() {
     }
 }
 
-fn initialize_fesi() {
-    if path::Path::new("FESI").is_dir() {
-        eprintln!("Fesi project already initialized");
-        process::exit(1)
+fn handle_run_command(args: &[String]) {
+    const RUN_HELP_MESSAGE: &str = r#"
+Runs testing on the endpoint provided
+
+Usage: fesi run [OPTIONS] <COMMAND>
+
+Options:
+    -m, --method        The HTTP method to run the test with (required)
+    -e, --endpoint      The http url for the service to test (required) 
+    -b, --body           JSON body for the request
+    -hd, --header        Request header
+    -h, --help        Prints the help message
+
+Examples:
+    fesi run -m GET -e http://localhost:8080/users
+    fesi run -m POST -e http://localhost:8080/data -b '{"key": "value"}' -h "Content-Type: application/json" -h "Authorization: Bearer token"
+"#;
+
+    // let mut endpoint: Option<String> = None;
+    // let mut method: Option<String> = None;
+    // let mut body: Option<HashSet<String, String>> = None;
+    // let mut header: Option<HashSet<String, String>> = None;
+    //
+    //
+    // if args.len() <= 1 {
+    //     println!("{RUN_HELP_MESSAGE}");
+    //     process::exit(1);
+    // }
+
+    for arg in args.iter() {
+        if arg == "-h" || arg == "--help" {
+            println!("Testing");
+            println!("{RUN_HELP_MESSAGE}");
+            process::exit(1);
+        }
     }
-    fs::create_dir("FESI").unwrap_or_else(|err| {
+    //
+    // for arg in args {
+    //     let value = arg.as_str();
+    //     match value {
+    //         "-h" | "--help" => {
+    //             println!("TESTING");
+    //             println!("{RUN_HELP_MESSAGE}");
+    //             process::exit(0)
+    //         }
+    //         _ => {
+    //             println!("{RUN_HELP_MESSAGE}");
+    //             process::exit(0)
+    //         }
+    //     }
+    // }
+}
+
+fn initialize_fesi_project() {
+    if path::Path::new(FESI_DIR_NAME).is_dir() {
+        eprintln!("Fesi project already initialized");
+        process::exit(0)
+    }
+    fs::create_dir(FESI_DIR_NAME).unwrap_or_else(|err| {
         eprintln!("Could not create Fesi project, {err}");
         process::exit(1)
     });
     println!("Fesi project created");
     process::exit(0)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn clean_up_dir() {
+        if path::Path::new(FESI_DIR_NAME).is_dir() {
+            fs::remove_dir_all(FESI_DIR_NAME).unwrap();
+        }
+    }
+
+    #[test]
+    fn test_initlize_fesi_success() {
+        clean_up_dir();
+
+        let result = std::panic::catch_unwind(|| {
+            initialize_fesi_project();
+        });
+
+        assert!(result.is_ok());
+        assert!(path::Path::new(FESI_DIR_NAME).is_dir());
+
+        clean_up_dir();
+    }
 }
